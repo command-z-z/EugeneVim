19aa9defa435ead0e9537914e170d2e449d0ff12
张宇健 <272181284@qq.com>
2022-08-18 00:04:39 +0800

    zsh config

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,6 +4,10 @@ cp ./.zshrc ~/.zshrc
 # git open
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 #zsh-syntax-highlighting 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 #zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+source ~/.zshrc
+
+echo "successful my zsh"
