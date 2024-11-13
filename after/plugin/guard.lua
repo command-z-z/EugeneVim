3f61bf29f3949aacd78eae2c0c0eb4d738ebd8f7
command-z-z <272181284@qq.com>
2024-11-13 19:58:10 +0800

    feat(guard): refactor guard setup and keymap

--- after/plugin/guard.lua
+++ after/plugin/guard.lua
@@ -1,8 +1,3 @@
-local statue, guard = pcall(require, "guard")
-if not statue then
-    return
-end
-
 local ft = require('guard.filetype')
 
 -- Assuming you have guard-collection
@@ -16,14 +11,14 @@ ft('lua'):fmt('lsp')
 ft('*'):lint('codespell')
 
 -- Call setup() LAST!
-guard.setup({
+vim.g.guard_config = {
     -- Choose to format on every write to a buffer
     fmt_on_save = false,
     -- Use lsp if no formatter was defined for this filetype
     lsp_as_default_formatter = false,
     -- By default, Guard writes the buffer on every format
     -- You can disable this by setting:
-    -- save_on_fmt = false,
-})
+    save_on_fmt = false,
+}
 
-vim.api.nvim_set_keymap("n", "<leader>fm", "<cmd>GuardFmt<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fm", "<cmd>Guard fmt<CR>", { silent = true, noremap = true })
